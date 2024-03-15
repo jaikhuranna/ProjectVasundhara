91f4ca3e99331e70cf9e29e607e02d7f32d5d576
Asmit Singh Chauhan <blr.asmit@gmail.com>
2024-03-15 14:54:16 +0530

    Removed all refill scripts

--- Assets/Scripts/InventorySystem/RefillManager.cs
+++ Assets/Scripts/InventorySystem/RefillManager.cs
@@ -1,1 +1,22 @@
-        Instantiate(mainElement1, mE1Pos, transform.rotation);
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RefillManager : MonoBehaviour
+{
+    public GameObject elemetHolder;
+    public GameObject element;
+
+    private Vector3 elementHolderPos; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        // elementHolderPos
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
