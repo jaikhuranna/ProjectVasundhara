f7440fca70efb1ec3900605f0330f204b919c4e4
Samarth Ale <161304725+Ale1as@users.noreply.github.com>
2024-03-16 02:12:08 +0530

    collision Detection
    - ocean orb and soil orb when collided with the ground forms a platform

--- Assets/macro_element_throw.cs
+++ Assets/macro_element_throw.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class macro_element_throw : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject GroundMesh;
+    public ParticleSystem PSforORB;
+    private void OnCollisionEnter(Collision other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            GroundMesh.SetActive(true);
+            PSforORB.Play();
+            Destroy(gameObject,2);
+        }
     }
 }
