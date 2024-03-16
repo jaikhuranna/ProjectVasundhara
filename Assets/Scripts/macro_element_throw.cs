5ef60940b66d0bfe49e1b0abfeb42fd4574c8461
Samarth Ale <161304725+Ale1as@users.noreply.github.com>
2024-03-16 15:23:21 +0530

    sun orb working

--- Assets/Scripts/macro_element_throw.cs
+++ Assets/Scripts/macro_element_throw.cs
@@ -16,5 +16,19 @@ public class macro_element_throw : MonoBehaviour
             ThrowParticle.Play();
             Destroy(gameObject,1f);
         }
+
+        if (other.gameObject.CompareTag("Sky"))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(0,2500f,0);
+            StartCoroutine(sunBurst());
+            //Destroy(gameObject,3f);
+        }
+    }
+
+    IEnumerator sunBurst()
+    {
+        
+        yield return new WaitForSeconds(3f);
+        ThrowParticle.Play();
     }
 }
