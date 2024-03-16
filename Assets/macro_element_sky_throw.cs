399e62a30834f03e1aa44a684c8b6ef6123a19f6
Samarth Ale <161304725+Ale1as@users.noreply.github.com>
2024-03-16 16:58:18 +0530

    all orbs added some tweaking to be done

---
+++ Assets/macro_element_sky_throw.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class macro_element_sky_throw : MonoBehaviour
+{
+
+    public ParticleSystem burstParticle; 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Sky"))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(0,2500f,0);
+            StartCoroutine(sunBurst());
+            //Destroy(gameObject,3f);
+        }
+    }
+
+    IEnumerator sunBurst()
+    {
+  
+        yield return new WaitForSeconds(5f);
+        burstParticle.Play();
+    }
+}
